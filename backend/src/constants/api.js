e98321165d0390c142e1ca7602e7841f48054fbc
Hanseul Shim <hanseulshim@gmail.com>
2020-07-14 14:17:33 -0400

    Update everything to typscript

--- backend/src/constants/api.js
+++
@@ -1,6 +0,0 @@
-const covidTrackingApi = 'https://covidtracking.com'
-module.exports = {
-	historicUs: `${covidTrackingApi}/api/v1/us/daily.json`,
-	currentUs: `${covidTrackingApi}/api/v1/us/current.json`,
-	historicState: `${covidTrackingApi}/api/v1/states/daily.json`
-}
--- backend/src/constants/tableNames.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	state: 'state',
-	state_week: 'state_week',
-	state_day: 'state_day',
-	country_day: 'country_day'
-}
